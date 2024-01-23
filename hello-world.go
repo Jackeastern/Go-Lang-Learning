c1a4077795168aa9c3b702a359a150969ce6822a
Jacques <jacques@jawisoftbcd.com>
2024-01-23 21:17:32 +0200

    Golang hello world.
    - Learned about package
    - Learned about import.
    - Learned how to use fmt.

---
+++ hello-world.go
@@ -0,0 +1,7 @@
+package main // Usign main package I guess.
+
+import "fmt" // Imporing fmt that is picked up from packages I guess.
+
+func main() {
+	fmt.Println("hello world") // Able to use fmt now because it's imported.
+}
